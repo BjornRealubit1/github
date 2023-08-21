1daae5f7eb8e5e71491627ed0ebef82c7c311e33
Bjorn_Realubit <112920127+BjornRealubit1@users.noreply.github.com>
2023-08-22 00:16:46 +0800

    Update errorsAndFunctions.sol

--- errorsAndFunctions.sol
+++ errorsAndFunctions.sol
@@ -1,46 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-contract MyToken {
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-    constructor(){
-        owner = msg.sender;
+contract ErrorHandling{
+    uint256 public value;
+
+    // Function using require
+    function valueRequried(uint256 _newValue) public {
+        require(_newValue > value, "Value is greater than new Value: Input another value");
+        value = _newValue;
     }
 
-    // public variables here
-    string public tokenName = "COIN";
-    string public tokenAbbrv = "CN";
-    uint public totalSupply = 0; 
-    address public owner;
 
-    // mapping variable here
-    mapping(address => uint) public balances;
-
-    modifier Owner{
-        assert(msg.sender == owner);
-        _;
-    }
-
-    // mint function
-    function mint (address _address, uint _value) public Owner{
-        totalSupply += _value; 
-        balances [_address] += _value;
-        assert(totalSupply >= _value);
-    }
-    
-    // burn function
-    function burn (address _address, uint _value) public Owner{
-        if (balances[_address] < _value) {
-            revert("Insufficient balance");
+    // Function using revert
+    function decrementValue(uint256 _decrementAmount) public {
+        if (_decrementAmount > value) {
+            revert("Cannot decrement more than the current value");
         }
-        totalSupply -= _value; 
-        balances [_address] -= _value;
-        assert(totalSupply <= type(uint).max - _value);
+        value -= _decrementAmount;
     }
 
-    function transfer (address _reciever, uint _value) public{
-        require(balances[msg.sender] >= _value, "Amount balance insufficient for transfer value!");
-        balances[msg.sender] -= _value;
-        balances[_reciever] += _value;
+    
+    // Function using assert 
+     function valueAssert(uint256 _newValue) public {
+        uint256 oldValue = value;
+        value = _newValue;
+        assert(value > oldValue);
     }
 }
