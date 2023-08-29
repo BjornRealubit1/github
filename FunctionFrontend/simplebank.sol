ffea494d636697a772c00d0811a6806047076416
Bjorn_Realubit <112920127+BjornRealubit1@users.noreply.github.com>
2023-08-29 09:45:55 +0800

    Update simplebank.sol

--- FunctionFrontend/simplebank.sol
+++ FunctionFrontend/simplebank.sol
@@ -3,18 +3,34 @@ pragma solidity 0.8.19;
 
 contract SimpleBank {
     mapping(address => uint256) private balances;
+    uint256 public mousePrice = 1 ether;
+    uint256 public keyboardPrice = 1 ether;
+    uint256 public monitorPrice = 1 ether;
 
-    function deposit() public payable {
+
+    function buyItem(uint256 itemId, uint256 quantity) public payable {
+        require(itemId >= 1 && itemId <= 3, "Invalid item ID");
+        uint256 itemPrice;
+        if (itemId == 1) {
+            itemPrice = mousePrice;
+        } else if (itemId == 2) {
+            itemPrice = keyboardPrice;
+        } else if (itemId == 3) {
+            itemPrice = monitorPrice;
+        }
+
+        require(
+            msg.value >= itemPrice * quantity,
+            "Insufficient funds to buy the items"
+        );
+
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
-    }
 
-    function getBalance() public view returns (uint256) {
-        return balances[msg.sender];
+    function setItemPrices(uint256 _mousePrice, uint256 _keyboardPrice, uint256 _monitorPrice) public {
+        mousePrice = _mousePrice;
+        keyboardPrice = _keyboardPrice;
+        monitorPrice = _monitorPrice;
     }
 }
